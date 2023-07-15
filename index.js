07dd861675c4a5f97028ccd5fe060397c9399f0a
Mansab Hashim <112323899+MansabHashim@users.noreply.github.com>
2023-07-15 11:50:18 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+let randomNumber1 = Math.floor(Math.random()*6)+1;
+
+let Imagecource1 = "images/dice" + randomNumber1 + ".png";
+
+let image1 = document.querySelectorAll("img")[0].setAttribute("src", Imagecource1);
+
+
+let randomNumber2 = Math.floor(Math.random()*6)+1;
+
+let Imagecource2 = "images/dice" + randomNumber2 + ".png";
+
+let image2 = document.querySelectorAll("img")[1].setAttribute("src", Imagecource2);
+
+
+
+
+if (randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "🚩 Player 1 wins :) ";
+} else if (randomNumber1 < randomNumber2){
+    document.querySelector("h1").innerHTML = " Player 2 wins :) 🚩 ";
+} else{
+    document.querySelector("h1").innerHTML = " (: Draw :) ";
+}
